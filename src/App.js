eaea033dfbbfa87111828f158c777bb78f8bc9f0
Sofia Genchi <sofigen16@gmail.com>
2023-05-29 16:05:37 -0300

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <Router>
       <Header />
+
+      <Routes>
+        <Route path='/' exact Component={Home} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className="box-uno">
+            <h1>SG</h1>
+        </div>
+
+        <div className="box-dos">
+            <h2>FrontEnd Web Developer</h2>
+            <p>Como desarrolladora web frontend certificada, utilizo ReactJS, JavaScript, HTML5 y CSS3 para crear sitios web interactivos. Mi enfoque se centra en ofrecer experiencias de usuario excepcionales a través de interfaces intuitivas y eficientes.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
